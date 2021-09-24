8565dc71778ec43485da8f9bfaf6d8eab9475801
arsan13 <arsan.anbu97@gmail.com>
2021-09-24 22:09:51 +0530

    Initial Commit

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Hello World</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default App;
