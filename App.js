79c3e71accf5e026e33e78322f5c17725eca5dde
arsan13 <arsan.anbu97@gmail.com>
2021-09-26 22:40:18 +0530

    Added Reminder Functionality

--- App.js
+++ App.js
@@ -1,25 +1,121 @@
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {
+  Button,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  ScrollView,
+} from 'react-native';
 import {Calendar} from 'react-native-calendars';
 
 const App = () => {
+  const [reminder, setReminder] = useState([]);
+  const [showForm, setShowForm] = useState(false);
+  const [selectedDate, setSelectedDate] = useState('');
+  const [note, setNote] = useState('');
+
+  const handlePress = val => {
+    setSelectedDate(val.dateString);
+    setShowForm(true);
+  };
+
+  const handleSave = () => {
+    if (note.length > 0) {
+      setReminder(prev => [
+        ...prev,
+        {
+          date: selectedDate,
+          note: note,
+        },
+      ]);
+    }
+
+    setSelectedDate('');
+    setNote('');
+    setShowForm(false);
+  };
+
+  const handleCancel = () => {
+    setSelectedDate('');
+    setNote('');
+    setShowForm(false);
+  };
+
   return (
-    <Calendar
-      hideExtraDays
-      enableSwipeMonths
-      markedDates={{
-        '2021-09-16': {selected: true, marked: true, selectedColor: 'blue'},
-        '2021-09-17': {marked: true},
-        '2021-09-18': {marked: true, dotColor: 'red', activeOpacity: 0},
-        '2021-09-19': {disabled: true, disableTouchEvent: true},
-      }}
-      onDayPress={e => {
-        console.log('selected day', e.dateString);
-      }}
-    />
+    <View>
+      <View>
+        <Calendar
+          hideExtraDays
+          enableSwipeMonths
+          onDayPress={val => handlePress(val)}
+          // markedDates={{
+          //   '2021-09-16': {selected: true, marked: true, selectedColor: 'blue'},
+          // }}
+        />
+      </View>
+
+      <View style={{paddingVertical: 10}}>
+        {!showForm ? (
+          <ScrollView>
+            <Text style={styles.reminderHeading}>Reminders</Text>
+            {reminder.length > 0 ? (
+              reminder.map((item, index) => (
+                <Text key={index} style={styles.reminders}>
+                  {item.date} {'  '} {item.note}
+                </Text>
+              ))
+            ) : (
+              <Text style={styles.reminders}>No reminders added</Text>
+            )}
+          </ScrollView>
+        ) : (
+          <View>
+            <Text style={styles.reminderHeading}>Add A Reminder</Text>
+            <Text style={{padding: 10, fontSize: 20}}>{selectedDate}</Text>
+            <TextInput
+              style={styles.input}
+              onChangeText={text => setNote(text)}
+              value={note}
+              placeholder="Remind me to..."
+            />
+            <View style={styles.btnContainer}>
+              <Button title="Save" onPress={handleSave} />
+              <Button title="Cancel" onPress={handleCancel} color="red" />
+            </View>
+          </View>
+        )}
+      </View>
+    </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  reminderHeading: {
+    borderBottomColor: 'green',
+    borderBottomWidth: 5,
+    paddingBottom: 5,
+    fontSize: 40,
+  },
+  reminders: {
+    fontSize: 20,
+    paddingVertical: 15,
+    borderBottomColor: 'black',
+    borderBottomWidth: 1,
+  },
+  input: {
+    fontSize: 20,
+    height: 50,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+  btnContainer: {
+    paddingTop: 20,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+  },
+});
 
 export default App;
