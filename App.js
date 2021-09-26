bed19aa047f7550ad51b9607dea49ffd7d2ded9a
arsan13 <arsan.anbu97@gmail.com>
2021-09-26 09:50:24 +0530

    Basic Design

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+import {Calendar} from 'react-native-calendars';
 
 const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Hello World</Text>
-    </View>
+    <Calendar
+      hideExtraDays
+      enableSwipeMonths
+      markedDates={{
+        '2021-09-16': {selected: true, marked: true, selectedColor: 'blue'},
+        '2021-09-17': {marked: true},
+        '2021-09-18': {marked: true, dotColor: 'red', activeOpacity: 0},
+        '2021-09-19': {disabled: true, disableTouchEvent: true},
+      }}
+      onDayPress={e => {
+        console.log('selected day', e.dateString);
+      }}
+    />
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create({});
 
 export default App;
